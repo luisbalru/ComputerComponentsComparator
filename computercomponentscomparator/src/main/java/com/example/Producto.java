2e8b142f0d569668de8dd67fa32cde09b10c6869
luisbalru <balderasruizluis@gmail.com>
2018-05-15 20:12:40 +0200

    Mayor generalización de objeto producto, contiene array de ofertas

--- computercomponentscomparator/src/main/java/com/example/Producto.java
+++ computercomponentscomparator/src/main/java/com/example/Producto.java
@@ -1,17 +1,15 @@
 package com.example;
 
+import java.util.ArrayList;
+
 public class Producto{
   private String nombre;
-  private String precio_pcc;
   private String category;
   private String marca;
-  private String precio_gs;
-  private String precio_amazon;
-  private String pccomp_id;
-  private String gs_id;
-  private String amazon_id;
   private String img;
   private String descripcion;
+  private String ean;
+  private ArrayList<Oferta> ofertas;
   
   /**
    * Función que devuelve el nombre del producto.
@@ -29,93 +27,6 @@ public class Producto{
     this.nombre = nombre;
   }
   
-  /**
-   * Getter para el precio en PCComponentes.
-   * @return String con el precio del producto en PCComponentes.
-   */
-  public String getPrecio_pcc() {
-    return precio_pcc;
-  }
-  
-  /**
-   * Setter para el precio en PCComponentes.
-   * @param precio_pcc String con el precio del producto en PCComponentes.
-   */
-  public void setPrecio_pcc(String precio_pcc) {
-    this.precio_pcc = precio_pcc;
-  }
-  
-  /**
-   * Getter del precio del producto en Amazon.
-   * @return String con el precio del producto en Amazon.
-   */
-  public String getPrecio_amazon() {
-    return precio_amazon;
-  }
-  
-  /**
-   * Setter del precio del producto en Amazon.
-   * @param precio_amazon String con el precio del producto en Amazon.
-   */
-  public void setPrecio_amazon(String precio_amazon) {
-    this.precio_amazon = precio_amazon;
-  }
-  
-  /**
-   * Getter del precio del producto en Google Shopping.
-   * @return String con el precio del producto en Google Shopping.
-   */
-  public String getPrecio_gs() {
-    return precio_gs;
-  }
-  
-  /***  /
-   * Getter del id del producto en PCComponentes.
-   * @return String con el id del producto en PCComponentes.
-   */
-  public String getPccomp_id() {
-    return pccomp_id;
-  }
-  
-  /**
-   * Setter del id del producto en PCComponentes.
-   * @param pccomp_id String con el id del producto en PCComponentes.
-   */
-  public void setPccomp_id(String pccomp_id) {
-    this.pccomp_id = pccomp_id;
-  }
-  
-  /**
-   * Getter del id del producto en Google Shopping.
-   * @return String con el id del producto en Google Shopping.
-   */
-  public String getGs_id() {
-    return gs_id;
-  }
-  
-  /**
-   * Setter del id del producto en Google Shopping.
-   * @param gs_id String con el id del producto en Google Shopping.
-   */
-  public void setGs_id(String gs_id) {
-    this.gs_id = gs_id;
-  }
-  
-  /**
-   * Getter del id del producto en Amazon.
-   * @return String con el id del producto en Amazon.
-   */
-  public String getAmazon_id() {
-    return amazon_id;
-  }
-  
-  /**
-   * Setter del id del producto en Amazon.
-   * @param amazon_id String con el id del producto en Amazon.
-   */
-  public void setAmazon_id(String amazon_id) {
-    this.amazon_id = amazon_id;
-  }
   
   /**
    * Getter de la url de la imagen del producto.
@@ -180,4 +91,22 @@ public class Producto{
   public void setMarca(String marca) {
 	this.marca = marca;
   }
+
+  public ArrayList<Oferta> getOfertas() {
+	return ofertas;
+  }
+
+  public void setOfertas(ArrayList<Oferta> ofertas) {
+	this.ofertas = ofertas;
+  }
+
+public String getEan() {
+	return ean;
+}
+
+public void setEan(String ean) {
+	this.ean = ean;
+}
+  
+  
 }
